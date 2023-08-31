d66a52039994b464aeec14c102a4f5805eb34b32
Daithi <71025690+tower977@users.noreply.github.com>
2023-09-01 00:17:41 +0800

    Add files via upload
    Added additional features

--- app.py
+++ app.py
@@ -68,6 +68,64 @@ def main():
         sns.countplot(data=hr_data, y='RecruitmentSource', ax=ax4, order=hr_data['RecruitmentSource'].value_counts().index)
         st.pyplot(fig4)
 
+    elif page == "Performance Analysis":
+        st.header("Performance Analysis")
+
+        # Distribution of performance scores across departments
+        st.subheader("Performance Score Distribution by Department")
+        fig1, ax1 = plt.subplots(figsize=(10, 6))
+        sns.countplot(data=hr_data, x='PerformanceScore', hue='Department', ax=ax1)
+        st.pyplot(fig1)
+
+        # Correlation between engagement survey results and performance scores
+        st.subheader("Engagement Survey vs. Performance Score")
+        fig2, ax2 = plt.subplots(figsize=(10, 6))
+        sns.boxplot(data=hr_data, x='PerformanceScore', y='EngagementSurvey', ax=ax2)
+        st.pyplot(fig2)
+
+        # Relationship between special projects count and performance score
+        st.subheader("Special Projects Count vs. Performance Score")
+        fig3, ax3 = plt.subplots(figsize=(10, 6))
+        sns.boxplot(data=hr_data, x='PerformanceScore', y='SpecialProjectsCount', ax=ax3)
+        st.pyplot(fig3)
+
+    elif page == "Managerial Analysis":
+        st.header("Managerial Analysis")
+
+        # Number of employees under each manager
+        st.subheader("Number of Employees by Manager")
+        fig1, ax1 = plt.subplots(figsize=(10, 8))
+        sns.countplot(data=hr_data, y='ManagerName', ax=ax1, order=hr_data['ManagerName'].value_counts().index)
+        st.pyplot(fig1)
+
+        # Average performance score of employees under each manager
+        st.subheader("Average Engagement Survey Score by Manager")
+        fig2, ax2 = plt.subplots(figsize=(10, 8))
+        sns.barplot(data=hr_data, y='ManagerName', x='EngagementSurvey', ax=ax2, ci=None)
+        st.pyplot(fig2)
+
+        # Employee satisfaction score distribution by manager
+        st.subheader("Employee Satisfaction by Manager")
+        fig3, ax3 = plt.subplots(figsize=(10, 8))
+        sns.boxplot(data=hr_data, y='ManagerName', x='EmpSatisfaction', ax=ax3)
+        st.pyplot(fig3)
+
+    elif page == "Attendance Analysis":
+        st.header("Attendance Analysis")
+
+        # Distribution of the number of times employees were late in the last 30 days
+        st.subheader("Distribution of Lateness in the Last 30 Days")
+        fig1, ax1 = plt.subplots(figsize=(10, 6))
+        sns.histplot(hr_data['DaysLateLast30'], kde=True, ax=ax1)
+        st.pyplot(fig1)
+
+        # Relationship between lateness and performance scores
+        st.subheader("Lateness vs. Performance Score")
+        fig2, ax2 = plt.subplots(figsize=(10, 6))
+        sns.boxplot(data=hr_data, x='PerformanceScore', y='DaysLateLast30', ax=ax2)
+        st.pyplot(fig2)
+
+
     # ... You can continue this structure for Performance Analysis, Managerial Analysis, and Attendance Analysis
 
 if __name__ == "__main__":
